a333544c19453d814a3dd500f2dd7a12d8e2d240
caio <caiofaria2308@gmail.com>
2019-03-22 10:05:05 -0300

    introducao

---
+++ aula1- introducao.py
@@ -0,0 +1,29 @@
+print ("Hello word")
+print ("Aula mundo")
+print (2+2)
+print (2*2)
+print (10/2)
+print (10*2)
+print (2**3)
+print (10%3)
+# Agora variaveis
+teste="Teste de variaveis"
+print (teste)
+# aprofundamento
+var1=1 #int
+var2=1.5 #float
+var3="Eu sou uma string" #string
+var4=True
+print (var1)
+print (var2)
+print (var3)
+print (var4)
+
+#Operadores relacionais
+x=2
+y=3
+a=1
+b=1
+print ( x==y)
+print (a==b)
+
---
+++ aula2.py
@@ -0,0 +1,7 @@
+# -*- coding:utf-8 -*-
+a=5
+b=6
+if (a<b):
+    print("a é menor que b")
+else:
+    print ("a não é menor que b")
