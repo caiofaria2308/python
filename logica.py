cecec87fb8728693053aeabe2cf8cadcdb859776
Caio de Faria Nogueira <1901956@SALAS.AULAS>
2019-08-12 21:31:18 -0300

    teste

---
+++ logica.py
@@ -0,0 +1,12 @@
+def getFatorial(x):
+    n1=[]
+    n2=[]
+    for i in range (int(x)):
+        n1.append(int(input()))
+        n2.append(int(input()))
+    for u in range (int(x)):
+        print (n1[int(u)]*n2[int(u)] )
+fatorial=input()
+getFatorial(fatorial)
+
+
